fabbe2705f937d0695ce7631502b8962420d8258
Yurii <streltsovyurii86@gmal.com>
2021-04-25 21:35:17 +0300

    Add validationUpdateContact

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,6 +4,7 @@ const Contacts = require('../../model/contacts');
 const {
   validationCreateContact,
   validationUpdateContact,
+  validationUpdateStatusContact,
 } = require('./validationContact');
 
 //Route to get all contacts
@@ -114,40 +115,44 @@ router.patch('/:contactId', validationUpdateContact, async (req, res, next) => {
 });
 
 //Route to get contact by ID whith favorite field
-router.patch('/:contactId/favorite', async (req, res, next) => {
-  try {
-    const contact = await Contacts.updateStatusContact(
-      req.params.contactId,
-      req.body,
-    );
-    console.log(typeof req.body);
-    const isFavorite = Object.keys(req.body).some(el => el === 'favorite');
-    console.log(isFavorite);
-    if (!isFavorite) {
-      return res.status(400).json({
-        status: 'error',
-        code: 400,
-        data: { message: 'missing field favorite' },
-      });
+router.patch(
+  '/:contactId/favorite',
+  validationUpdateStatusContact,
+  async (req, res, next) => {
+    try {
+      const contact = await Contacts.updateStatusContact(
+        req.params.contactId,
+        req.body,
+      );
+      console.log(typeof req.body);
+      const isFavorite = Object.keys(req.body).some(el => el === 'favorite');
+      console.log(isFavorite);
+      if (!isFavorite) {
+        return res.status(400).json({
+          status: 'error',
+          code: 400,
+          data: { message: 'missing field favorite' },
+        });
+      }
+      if (contact) {
+        return res.json({
+          status: 'success',
+          code: 200,
+          data: {
+            contact,
+          },
+        });
+      } else {
+        return res.status(404).json({
+          status: 'error',
+          code: 404,
+          data: 'Not Found',
+        });
+      }
+    } catch (e) {
+      next(e);
     }
-    if (contact) {
-      return res.json({
-        status: 'success',
-        code: 200,
-        data: {
-          contact,
-        },
-      });
-    } else {
-      return res.status(404).json({
-        status: 'error',
-        code: 404,
-        data: 'Not Found',
-      });
-    }
-  } catch (e) {
-    next(e);
-  }
-});
+  },
+);
 
 module.exports = router;
--- routes/api/validationContact.js
+++ routes/api/validationContact.js
@@ -2,6 +2,7 @@ const Joi = require('joi');
 
 // Validation when creating a contact
 const schemaCreateContact = Joi.object({
+  favorite: Joi.boolean().optional(),
   name: Joi.string().min(3).max(30).required(),
   email: Joi.string().email().required(),
   phone: Joi.string()
@@ -22,6 +23,17 @@ const schemaUpdateContact = Joi.object({
     .optional(),
 }).min(1);
 
+// Validation when updating status of contact
+const schemaUpdateStatusContact = Joi.object({
+  favorite: Joi.boolean().required(),
+  name: Joi.string().min(3).max(30).optional(),
+  email: Joi.string().email().optional(),
+  phone: Joi.string()
+    .regex(/^[0-9]{10}$/)
+    .messages({ 'string.pattern.base': `Phone number must have 10 digits.` })
+    .optional(),
+});
+
 const validate = async (schema, obj, next) => {
   try {
     await schema.validateAsync(obj);
@@ -38,5 +50,8 @@ module.exports = {
   },
   validationUpdateContact: async (req, res, next) => {
     return await validate(schemaUpdateContact, req.body, next);
+  },
+  validationUpdateStatusContact: async (req, res, next) => {
+    return await validate(schemaUpdateStatusContact, req.body, next);
   },
 };
