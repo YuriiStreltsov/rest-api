38f16edfdadb046ff87de7ffc9ea9df530763b89
Yurii <streltsovyurii86@gmail.com>
2021-05-03 22:04:59 +0300

    Add users routers and controllers

---
+++ controllers/users.js
@@ -0,0 +1,11 @@
+const Users = require('../model/users');
+
+const signup = async (req, res, next) => {};
+const login = async (req, res, next) => {};
+const logout = async (req, res, next) => {};
+
+module.exports = {
+  signup,
+  login,
+  logout,
+};
---
+++ routes/users/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const ctrl = require('../../controllers/users');
+
+//  Route update a contact by ID
+router.post('/signup', ctrl.signup);
+router.post('/login', ctrl.login);
+router.post('/logout', ctrl.logout);
+
+module.exports = router;
