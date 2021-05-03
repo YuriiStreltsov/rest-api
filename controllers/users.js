9c13de5f0c09eb6895130a43bc8fe19ce5b9721b
Yurii <streltsovyurii86@gmail.com>
2021-05-03 23:01:32 +0300

    Add logic to "signup" function of "controllers"

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,34 @@
+const { HttpCode } = require('../helper/constants');
 const Users = require('../model/users');
 
-const signup = async (req, res, next) => {};
+const signup = async (req, res, next) => {
+  const { email } = req.body;
+  const user = await Users.findByEmail(email);
+  if (user) {
+    return res.status(HttpCode.CONFLICT).json({
+      status: 'error',
+      code: HttpCode.CONFLICT,
+      message: 'Email in use',
+    });
+  }
+
+  try {
+    const newUser = await Users.create(req.body);
+    return res.status(HttpCode.CREATED).json({
+      status: 'success',
+      code: HttpCode.CREATED,
+      data: {
+        user: {
+          email: newUser.email,
+          subscription: newUser.subscription,
+        },
+      },
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
 const login = async (req, res, next) => {};
 const logout = async (req, res, next) => {};
 
